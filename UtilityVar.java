81fa3d6fee18e9319b94155f8d07ead8c972d2f4
rlmar123 <rlmar123@yahoo.com>
2019-10-24 21:12:36 -0400

    Add files via upload
    I added the DB using sqllite. Getting the top 5 scores was not working with a db query, so I stored the DB in an Arraylist and let the Arraylist sort the data based on highest to lowest scores. Not efficient I know but the SQLite query was not sorting data properly

---
+++ UtilityVar.java
@@ -0,0 +1,15 @@
+package com.example.mathapp;
+
+public class UtilityVar {
+
+    //Database related items
+    public static final int DATABASE_VERSION = 1;
+    public static final String DATABASE_NAME = "mult_app_db";
+    public static final String TABLE_NAME = "player";
+
+    //Player table columns names
+    public static final String KEY_ID = "id";
+    public static final String KEY_NAME = "name";
+    public static final String KEY_SCORE = "score";
+
+}
